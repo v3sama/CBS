adbe1172923cba9b70bfc7339616c367cf9d7771
Manh Vinh <Manh Vinh@MANHVINH-PC>
2019-10-17 21:44:13 +0700

    layout

--- src/main/java/com/cbs/controllers/LayoutController.java
+++ src/main/java/com/cbs/controllers/LayoutController.java
@@ -1,8 +1,16 @@
 package com.cbs.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LayoutController {
 
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String index( ) {
+	
+		return "/admin/index";
+	}
 }
