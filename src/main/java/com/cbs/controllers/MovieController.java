d080c4f77272750188b51d1d377a8f081d099d46
ntloc9 <ntloc9@gmail.com>
2019-10-21 18:26:03 +0700

    add dependency

--- src/main/java/com/cbs/controllers/MovieController.java
+++ src/main/java/com/cbs/controllers/MovieController.java
@@ -1,24 +1,28 @@
 package com.cbs.controllers;
 
 import com.cbs.model.Movie;
+import com.cbs.model.MyReponse;
 import com.cbs.services.ActorService;
 import com.cbs.services.MovieService;
 import com.cbs.services.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class MovieController {
-
 
     private final MovieService movieService;
 
@@ -52,6 +56,12 @@ public class MovieController {
         Page<Movie> searchResult = movieService.searchByTittle(movieTittle, page);
         model.addAttribute("allMovie", searchResult);
         return "/movie";
+    }
+
+    @GetMapping("/api/movie")
+    public ResponseEntity<Object> getAllMovieApi() {
+        MyReponse<List<Movie>> response = new MyReponse<>("success", movieService.getAllMovies());
+        return new ResponseEntity<Object>(response, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/admin/add/movie", method = RequestMethod.GET)
