6d3aac54bf10348473a7c57e4480c35c666e35b2
ntloc9 <ntloc9@gmail.com>
2019-10-25 12:43:34 +0700

    thanh toan sap xong

--- src/main/java/com/cbs/controllers/BookTicketRestController.java
+++ src/main/java/com/cbs/controllers/BookTicketRestController.java
@@ -1,5 +1,7 @@
 package com.cbs.controllers;
 
+import com.cbs.dto.CustomUserDetail;
+import com.cbs.dto.ReviewOrderDTO;
 import com.cbs.dto.SeatReceiveDTO;
 import com.cbs.dto.SeatRenderDTO;
 import com.cbs.model.*;
@@ -8,6 +10,7 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.DayOfWeek;
@@ -26,9 +29,10 @@ public class BookTicketRestController {
     private final SeatServices seatServices;
     private final PriceService priceService;
     private final UserService userService;
+    private final OrderService orderService;
 
     @Autowired
-    public BookTicketRestController(MovieService movieService, MovieSessionService movieSessionService, CinemaService cinemaService, ProvinceService provinceService, TicketService ticketService, SeatServices seatServices, PriceService priceService, UserService userService) {
+    public BookTicketRestController(MovieService movieService, MovieSessionService movieSessionService, CinemaService cinemaService, ProvinceService provinceService, TicketService ticketService, SeatServices seatServices, PriceService priceService, UserService userService, OrderService orderService) {
         this.movieService = movieService;
         this.movieSessionService = movieSessionService;
         this.cinemaService = cinemaService;
@@ -37,10 +41,13 @@ public class BookTicketRestController {
         this.seatServices = seatServices;
         this.priceService = priceService;
         this.userService = userService;
+        this.orderService = orderService;
     }
 
     private Price priceVipObj;
     private Price priceThuongObj;
+    private List<String> listSelectedSeats;
+    private MovieSession xuatChieu;
 
     //Lấy ghế theo session id
 //    public @ResponseBody I
@@ -49,6 +56,7 @@ public class BookTicketRestController {
 
         //Lấy thông tin trong phòng chiếu theo suất chiếu
         MovieSession movieSession = movieSessionService.getSessionById(Long.parseLong(sessionId));
+        xuatChieu = movieSession;
         int screenRow = movieSession.getCinemaScreen().getRows();
         Long screenID = movieSession.getCinemaScreen().getId();
         Long movieId = movieSession.getMovie().getId();
@@ -73,6 +81,7 @@ public class BookTicketRestController {
             long remainder = seat.getId()%12;
             vipSeat.put(seat.getRow().getId(), (remainder==0?12l:remainder));
         }
+
         //Đổ 2 danh sách vào string seat map
         try {
             //Tạo List String Seat Map
@@ -132,7 +141,6 @@ public class BookTicketRestController {
             e.printStackTrace();
             System.out.println("->>> loi add price");
         }
-        //Đổ giá vào dto
 
         return seatDTO;
     }
@@ -150,6 +158,15 @@ public class BookTicketRestController {
 
     @PostMapping(value = "api/review")
     public String nhanGhe(@RequestBody SeatReceiveDTO seatReceiveDTO) throws Exception{
+        CustomUserDetail loggedInUser = (CustomUserDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userService.findById(loggedInUser.getUserId());
+        SOrder order = new SOrder();
+        order.setMember(user);
+        orderService.addOrder(order);
+        long orderId = order.getId();
+        //gán để lát đưa sang trang thanh toán
+        listSelectedSeats = seatReceiveDTO.getDataghe();
+
         try{
             Map<String, Integer> doubleBraceMap = new HashMap<String, Integer>() {{
                 put("A", 1);
@@ -177,24 +194,56 @@ public class BookTicketRestController {
                 long seatId = (rowIdHandle * 12) + seatAtRow;
                 Seat newSeat = seatServices.getSeatById(seatId);
                 MovieSession newMS = movieSessionService.getSessionById(sessionReceived);
-                User user = userService.findById(1l);
+
                 Ticket ticket = new Ticket();
                 ticket.setSeat(newSeat);
                 ticket.setMovieSession(newMS);
                 ticket.setMember(user);
                 if (newSeat.isVIP()) {
                     ticket.setPrice(priceVipObj);
+                    ticket.setAmount(priceVipObj.getPrice());
                 } else {
                     ticket.setPrice(priceThuongObj);
+                    ticket.setAmount(priceThuongObj.getPrice());
                 }
+
+                ticket.setOrder(orderService.getOrderByID(orderId));
                 ticketService.addTicket(ticket);
                 System.out.println(ticket.toString());
             }
 
         }catch (Exception e){
-
+            e.printStackTrace();
+            System.out.println("Loi Dat Ve");
         }
 
-        return "ok";
+        return "" + orderId;
     }
+
+    @GetMapping(value = "api/getMovieByOrder")
+    public ReviewOrderDTO getMovieByOrder(@RequestParam(value = "order") String orderId) throws Exception{
+        CustomUserDetail loggedInUser = (CustomUserDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        SOrder order = orderService.getOrderByID(Long.parseLong(orderId));
+        boolean compare =  order.getMember().getId().equals(loggedInUser.getUserId());
+//        if (compare){
+//
+//        }
+        ReviewOrderDTO reviewOrderDTO = new ReviewOrderDTO();
+        reviewOrderDTO.setGhe(listSelectedSeats);
+        reviewOrderDTO.setNgay(xuatChieu.getTime().toLocalDate().toString());
+        reviewOrderDTO.setSuatchieu(xuatChieu.getTime().toLocalTime().toString());
+        return reviewOrderDTO;
+    }
+
+    @GetMapping(value = "api/getMovieBySession")
+    public ReviewOrderDTO getMovieBySession(@RequestParam(value = "session") String orderId) throws Exception{
+        ReviewOrderDTO reviewOrderDTO = new ReviewOrderDTO();
+//        reviewOrderDTO.setGhe(listSelectedSeats);
+        reviewOrderDTO.setTenphim(xuatChieu.getMovie().getTitle());
+        reviewOrderDTO.setRap(xuatChieu.getCinemaScreen().getCinema().getTitle());
+        reviewOrderDTO.setNgay(xuatChieu.getTime().toLocalDate().toString());
+        reviewOrderDTO.setSuatchieu(xuatChieu.getTime().toLocalTime().toString());
+        return reviewOrderDTO;
+    }
+
 }
