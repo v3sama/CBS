e976b6c8f541dc24a82651038811549d8f24c1c8
v3sama <v3sama@gmail.com>
2019-10-25 10:44:47 +0700

    card

--- src/main/java/com/cbs/services/CardService.java
+++ src/main/java/com/cbs/services/CardService.java
@@ -1,8 +1,39 @@
 package com.cbs.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public interface CardService {
+import com.cbs.model.CardInformation;
+import com.cbs.repository.CardRepository;
+import com.cbs.repository.ProvinceRepository;
 
+@Service
+public class CardService {
+	private final CardRepository cardRepository;
+
+	@Autowired
+	public CardService(CardRepository cardRepository) {
+		this.cardRepository = cardRepository;
+	}
+	
+	public List<CardInformation> getAllCard() {
+		return cardRepository.findAll();
+	}
+
+	public CardInformation getCardByID(Long id) {
+		return cardRepository.getOne(id);
+	}
+
+
+	public void deleteCardByID(Long id) {
+		if (getCardByID(id) != null) {
+			cardRepository.deleteById(id);
+		}
+	}
+
+	public void addCard(CardInformation card) {
+		cardRepository.saveAndFlush(card);
+	}
 }
