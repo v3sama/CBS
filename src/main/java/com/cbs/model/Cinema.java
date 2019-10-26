149f3b03f453754ecb1c51ab73756b8fc25cb2cf
Manh Vinh <Manh Vinh@MANHVINH-PC>
2019-10-26 21:04:02 +0700

    a

--- src/main/java/com/cbs/model/Cinema.java
+++ src/main/java/com/cbs/model/Cinema.java
@@ -13,35 +13,24 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-
 import java.util.Set;
 
 @Getter
 @Setter
 @Entity
-@EqualsAndHashCode(callSuper = true,exclude = {"province","cinemaScreens"})
+@EqualsAndHashCode(callSuper = true, exclude = { "province", "cinemaScreens" })
 public class Cinema extends BaseEntity {
 	private static final long serialVersionUID = 1L;
-	@Column(unique=true)
-    private String title;
-    private String address;
-    private String phone;
-  
-    @ManyToOne
-    @JoinColumn(name = "province_id")
-    private Province province;
-    
-<<<<<<< HEAD
-    @OneToMany(mappedBy = "cinema")
-<<<<<<< HEAD
-=======
-   
-   @OneToMany(mappedBy = "cinema")
->>>>>>> refs/heads/dev
-    private Set<CinemaScreen> cinemaScreens;
+	@Column(unique = true)
+	private String title;
+	private String address;
+	private String phone;
 
-=======
-    private Set<CinemaRoom> cinemaRoomReferences;
->>>>>>> branch 'master' of https://github.com/v3sama/CBS
+	@ManyToOne
+	@JoinColumn(name = "province_id")
+	private Province province;
+
+	@OneToMany(mappedBy = "cinema")
+	private Set<CinemaScreen> cinemaScreens;
 
 }
